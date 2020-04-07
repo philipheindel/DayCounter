0cf71de167d29f8cf272f570f15c2398f2408be2
Philip Heindel <p.e.heindel@gmail.com>
2020-04-07 00:05:31 -0400

    Fixed name typos

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,16 @@
 const staticDayCounter = "day-counter-v1";
 const assets = [
-  "./DayCount/",
-  "./DayCount/index.html",
-  "./DayCount/css/style.css",
-  "./DayCount/js/script.js",
-  "./DayCount/lib/bootstrap-4.4.1-dist/css/bootstrap.min.css",
-  "./DayCount/lib/bootstrap-4.4.1-dist/css/bootstrap-grid.min.css",
-  "./DayCount/lib/bootstrap-4.4.1-dist/css/bootstrap-reboot.min.css",
-  "./DayCount/lib/jquery-3.4.1/js/jquery-3.4.1.slim.min.js",
-  "./DayCount/lib/popper-1.16.0-dist/js/popper.min.js",
-  "./DayCount/lib/bootstrap-4.4.1-dist/js/bootstrap.min.js",
-  "./DayCount/lib/bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js",
+  "./DayCounter/",
+  "./DayCounter/index.html",
+  "./DayCounter/css/style.css",
+  "./DayCounter/js/script.js",
+  "./DayCounter/lib/bootstrap-4.4.1-dist/css/bootstrap.min.css",
+  "./DayCounter/lib/bootstrap-4.4.1-dist/css/bootstrap-grid.min.css",
+  "./DayCounter/lib/bootstrap-4.4.1-dist/css/bootstrap-reboot.min.css",
+  "./DayCounter/lib/jquery-3.4.1/js/jquery-3.4.1.slim.min.js",
+  "./DayCounter/lib/popper-1.16.0-dist/js/popper.min.js",
+  "./DayCounter/lib/bootstrap-4.4.1-dist/js/bootstrap.min.js",
+  "./DayCounter/lib/bootstrap-4.4.1-dist/js/bootstrap.bundle.min.js",
 ];
 
 self.addEventListener("install", (installEvent) => {
