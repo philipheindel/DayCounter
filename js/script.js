db9f1011ae24fd9756e6411598adfd4ab1f24e48
Philip Heindel <p.e.heindel@gmail.com>
2020-04-06 23:56:30 -0400

    Moved service worker registration code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 const initialize = () => {
   setInner("#counter", getCount());
-
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-      navigator.serviceWorker
-        .register("./DayCount/serviceworker.js")
-        .then((res) => console.log("service worker registered"))
-        .catch((err) => console.log("service worker not registered", err));
-    });
-  }
 };
 
 function getCount(offset) {
@@ -41,4 +32,15 @@ function setInner(query, text) {
   _(query).innerHTML = text;
 }
 
+//#endregion
+
+//#region Service worker registration
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./DayCount/serviceworker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
 //#endregion
