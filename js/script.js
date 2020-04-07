013c67d7897d8b63fc4d41abe9329cab738d6ebc
Philip Heindel <p.e.heindel@gmail.com>
2020-04-07 00:19:15 -0400

    Updated getCount to account for daylight savings

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@ const initialize = () => {
   setInner("#counter", getCount());
 };
 
+/**
+ * The day of the year counter code was copied from the included Stackoverflow answer.
+ * https://stackoverflow.com/questions/8619879/javascript-calculate-the-day-of-the-year-1-366
+ * 
+ * @param {number} offset Number of days to offset from the count.
+ */
 function getCount(offset) {
   if (offset == null) {
     offset = 0;
   }
   let now = new Date();
   let start = new Date(now.getFullYear(), 0, 0);
-  let diff = now - start;
+  let diff = now - start + (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
   let oneDay = 1000 * 60 * 60 * 24;
   return Math.floor(diff / oneDay) + offset;
 }
